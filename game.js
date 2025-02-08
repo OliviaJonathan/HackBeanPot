aeae7d586ccb1bc4bfe52dd9e60f184f5b2b0356
alyssa najera <anajera@bu.edu>
2025-02-08 13:33:09 -0500

    added comments

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 function awake(){
     console.log("Awake");
+    // first time waking up capybara
     document.getElementById("capybara").src = capybara.src.replace("src/capybaraasleep.png", "src/capybaraawake.png");
+    // take out hint
     document.getElementById("hint").innerHTML = "";
     document.getElementById("hint").style = "padding: 3.6%";
 }
@@ -9,6 +11,7 @@ document.addEventListener('keydown', function(event) {
     // Log the key that was pressed
     console.log("Key pressed: " + event.key)
 
+    // if z is clicked, capybara goes to sleep
     if(event.key === "z") {
         document.getElementById("capybara").src = capybara.src.replace("src/capybaraawake.png", "src/capybaraasleep.png");
     }
