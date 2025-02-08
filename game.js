38e6a9cf9460884f3fa943c19aee6899e6978a96
alyssa najera <anajera@bu.edu>
2025-02-08 13:31:33 -0500

    added wake up and sleep functions

--- game.js
+++ game.js
@@ -1,4 +1,16 @@
 function awake(){
     console.log("Awake");
     document.getElementById("capybara").src = capybara.src.replace("src/capybaraasleep.png", "src/capybaraawake.png");
+    document.getElementById("hint").innerHTML = "";
+    document.getElementById("hint").style = "padding: 3.6%";
 }
+
+document.addEventListener('keydown', function(event) {
+    // Log the key that was pressed
+    console.log("Key pressed: " + event.key)
+
+    if(event.key === "z") {
+        document.getElementById("capybara").src = capybara.src.replace("src/capybaraawake.png", "src/capybaraasleep.png");
+    }
+
+});
