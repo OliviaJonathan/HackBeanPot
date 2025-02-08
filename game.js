195c479b4ec3649e68a821380b3c2c66dc9e4bad
OliviaJonathan <oliviajonathan11@gmail.com>
2025-02-08 14:59:50 -0500

    added the stand function

--- game.js
+++ game.js
@@ -13,6 +13,35 @@ function awake(){
     document.getElementById("hint").style = "padding: 3.6%";
 }
 
+// FART FUNCTION
+const capybara = document.getElementById('capybara');
+const sound = document.getElementById('fart');
+capybara.addEventListener('click', function(){
+        const imageRect = capybara.getBoundingClientRect();
+        const clickX = event.clientX - imageRect.left;
+        const clickY = event.clientY - imageRect.top;
+
+        const clickArea = {
+            x: 80,   // Starting X coordinate
+            y: 100,   // Starting Y coordinate
+            width: 30, // Width of the clickable area
+            height: 20 // Height of the clickable area
+        };
+
+        // Check if the click is within the defined area
+        if (clickX >= clickArea.x && clickX <= clickArea.x + clickArea.width &&
+            clickY >= clickArea.y && clickY <= clickArea.y + clickArea.height) {
+            // Reset time of audio
+            sound.currentTime = 0;
+            // Play the sound
+            sound.play();
+        }
+
+        console.log("Fart");
+})
+
+
+
 
 
 document.addEventListener('keydown', function(event) {
@@ -23,10 +52,9 @@ document.addEventListener('keydown', function(event) {
     if(event.key === "z") {
         document.getElementById("capybara").src = capybara.src.replace("src/capybaraawake.png", "src/capybaraasleep.png");
     }
-    //if s is clicked, capybara stands up
-    if(event.key === "s"){
-        document.getElementById("capybara").scr = capybara.src.replace("src/capybaraawake.png", "src/capybarastand.png")
+    // if s is clicked, capybara stands
+    if(event.key === "s") {
+        document.getElementById("capybara").src = capybara.src.replace("src/capybaraawake.png", "src/capybarastand.png");
     }
-   
 
 });
