39a3d70b7ada69e14ef8685d27c743f1326236cb
alyssa najera <anajera@bu.edu>
2025-02-08 13:42:47 -0500

    try adding sounds

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
+var snd = new Audio("src/boom.mp3");
+
 function awake(){
     console.log("Awake");
     // first time waking up capybara
     document.getElementById("capybara").src = capybara.src.replace("src/capybaraasleep.png", "src/capybaraawake.png");
+    snd.play();
     // take out hint
     document.getElementById("hint").innerHTML = "";
     document.getElementById("hint").style = "padding: 3.6%";
