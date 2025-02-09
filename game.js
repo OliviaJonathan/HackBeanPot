a4d9da09e152ddf5fcf6ef5d55e8221d315ed64b
sstangg <sstang@bu.edu>
2025-02-08 21:12:03 -0500

    add drag and drop hat functionality

--- game.js
+++ game.js
@@ -103,11 +103,69 @@ document.addEventListener('keydown', function(event) {
 
 });
 
-// drag & drop
-let newX = 0, newY=0;
-const hat = document.getElementById("floppy");
-/*hat adder*/
-function addHat(){
 
+// Make the DIV element draggable:
+//dragElement(document.getElementById("floppy"));
+// Apply dragElement to each element with class "hat"
+document.querySelectorAll('.hat').forEach(function (el) {
+    dragElement(el);
+});
 
+function dragElement(elmnt) {
+
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        if (elmnt.id === "wig"){
+            elmnt.style.width = "10vw";
+        }
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        const x = event.clientX;
+        const y = event.clientY;
+        if (elmnt.id === "wig" && 174 <= x && x <= 234 && 192 <= y && y<= 423){
+            elmnt.style.width = "5vw";
+        }
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
+
+/* to help with finding coordinates of div
+const coordinatesDiv = document.getElementById('coordinates');
+
+document.addEventListener('mousemove', function(event) {
+    const x = event.clientX;
+    const y = event.clientY;
+    coordinatesDiv.textContent = `Mouse X: ${x}, Mouse Y: ${y}`;
+});*/
